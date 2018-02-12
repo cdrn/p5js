5de52435361b4b32ae9c6aacf0c1eaf680cccd30
Chris Doran <chrisdoran@protonmail.com>
2018-02-12 19:16:28 +1000

    fun snake visualisation

--- genetic_algorithm/sketch.js
+++ genetic_algorithm/sketch.js
@@ -15,7 +15,8 @@ function draw () {
 		nodes[j].place(nodes);
 	}
 	for (k = 0; k < numNodes; k++) {
-		nodes[k].disp();	
+		nodes[k].disp();
+		nodes[k].move();	
 	}
 }
 
@@ -23,6 +24,8 @@ function circObj(d, id){
 	this.x = random(width)
 	this.y = random(height)
 	this.d = d
+	this.accelerationX = random(-1.0, 1.0)
+	this.accelerationY = random(-1.0, 1.0)
 	this.id = id;
 	this.color = color(random(255),random(255),random(255))
 	this.hit = true;
@@ -30,11 +33,9 @@ function circObj(d, id){
 	this.place = function(objArray){
 
 			for(i=0;i<objArray.length;i++){
-				if(this.id != i){ //dont do the check if it is looking at itself
-
+				if( this.id != i ){ //dont do the check if it is looking at itself
 					this.hit = collideCircleCircle(this.x, this.y, this.d, objArray[i].x, objArray[i].y, objArray[i].d); //colliding with anything?
-
-					if(this.hit == true){ // if we ever get a true we have to try again, this works since we iterate down through the objects one by one.
+					if( this.hit == true ){ // if we ever get a true we have to try again, this works since we iterate down through the objects one by one.
 						//try again:
 						this.x = random(width)
 						this.y = random(height)
@@ -42,12 +43,14 @@ function circObj(d, id){
 				}
 			}
 	}
-
 	this.disp = function(){
 		noStroke();
 		fill(this.color);
 		ellipse(this.x,this.y,this.d,this.d);
-
+	}
+	this.move = function(){
+		this.x = this.x + this.accelerationX
+		this.y = this.y + this.accelerationY
 	}
 
 }
